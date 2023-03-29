dabad30207529851dccb71286ff2f7cd508afc34
mario <mariobassem12@gmail.com>
2023-03-29 09:00:18 +0200

    wip: implement machines & k8s procedures
    Co-authored-by: Omar Abdulaziz <omarabdul3ziz@gmail.com>

--- pkg/server/procedures/k8s.go
+++ pkg/server/procedures/k8s.go
@@ -3,15 +3,44 @@ package procedure
 import (
 	"context"
 
+	"github.com/threefoldtech/grid3-go/deployer"
+	"github.com/threefoldtech/grid3-go/workloads"
 	"github.com/threefoldtech/tf-grid-cli/pkg/server/types"
 )
 
-func K8sDeploy(ctx context.Context, cluser types.K8sCluster) (types.K8sCluster, error)
+func K8sDeploy(ctx context.Context, cluster types.K8sCluster, client deployer.TFPluginClient) (types.K8sCluster, error) {
+	// validate project name is unique
+	// map to workloads.k8sCluster
+	// use k8sDeployer to deploy cluster
+	// construct result
+	k8s := workloads.K8sCluster{}
 
-func K8sDelete(ctx context.Context, clusterName string) error
+	//master name must be: projectName.master
+	//worker names must be: projectName.workerX
+	client.K8sDeployer.Deploy(ctx)
+}
+
+func K8sDelete(ctx context.Context, clusterName string) error {
+	// get all contracts by project name
+	// client.SubstrateConn.CancelContract(client.Identity, contractID)
+}
 
 func K8sAddNode(ctx context.Context, cluserName string, node types.K8sNode) (types.K8sCluster, error)
 
 func K8sRemoveNode(ctx context.Context, clusterName string, nodeName string) (types.K8sCluster, error)
 
-func K8sGet(ctx context.Context, clusterName string) (types.K8sCluster, error)
+func K8sGet(ctx context.Context, clusterName string, client deployer.TFPluginClient) (types.K8sCluster, error) {
+	// get all contracts by project name
+	masterMap := map[uint32]string{}
+	workerMap := map[uint32][]string{}
+	contracts, err := client.ContractsGetter.ListContractsOfProjectName(clusterName)
+	nodeClient, err := client.NcPool.GetNodeClient(client.SubstrateConn, contracts.NodeContracts[0].NodeID)
+	dl, err := nodeClient.DeploymentGet(ctx, contractID)
+	for _, w := range dl.Workloads {
+		// if name indicates master, assign to master map
+		// if name indicates worker, append to worker map
+	}
+
+	//
+	client.State.LoadK8sFromGrid()
+}
--- pkg/server/procedures/machines.go
+++ pkg/server/procedures/machines.go
@@ -2,13 +2,89 @@ package procedure
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/pkg/errors"
 	"github.com/threefoldtech/grid3-go/deployer"
+	"github.com/threefoldtech/grid3-go/workloads"
 	"github.com/threefoldtech/tf-grid-cli/pkg/server/types"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
 )
 
-// machines.deploy
-func MachinesDeploy(ctx context.Context, model types.MachinesModel, client deployer.TFPluginClient) (types.MachinesModel, error)
+// nodes should always be provided
+func MachinesDeploy(ctx context.Context, model types.MachinesModel, client deployer.TFPluginClient) (types.MachinesModel, error) {
+	// validation
+	contracts, err := client.ContractsGetter.ListContractsOfProjectName(model.Name)
+	if err != nil {
+		return types.MachinesModel{}, errors.Wrapf(err, "failed to retreive contracts with project name %s", model.Name)
+	}
+
+	if len(contracts.NameContracts) > 0 || len(contracts.NodeContracts) > 0 || len(contracts.RentContracts) > 0 {
+		return types.MachinesModel{}, fmt.Errorf("project name %s is not unique", model.Name)
+	}
+
+	// if machines don't have nodes assigned, should be assigned here
+
+	// deploy network
+	nodeList := []uint32{}
+	nodeMachineMap := map[uint32][]types.Machine{}
+	for _, machine := range model.Machines {
+		if _, ok := nodeMachineMap[machine.NodeID]; !ok {
+			nodeList = append(nodeList, machine.NodeID)
+		}
+		nodeMachineMap[machine.NodeID] = append(nodeMachineMap[machine.NodeID], machine)
+	}
+
+	ipRange, err := gridtypes.ParseIPNet(model.Network.IPRange)
+	if err != nil {
+		return types.MachinesModel{}, errors.Wrapf(err, "network ip range (%s) is invalid", model.Network.IPRange)
+	}
+
+	znet := workloads.ZNet{
+		Name:        model.Network.Name,
+		Nodes:       nodeList,
+		IPRange:     ipRange,
+		AddWGAccess: model.Network.AddWireguardAccess,
+	}
+
+	err = client.NetworkDeployer.Deploy(ctx, &znet)
+	if err != nil {
+		return types.MachinesModel{}, errors.Wrap(err, "failed to deploy network")
+	}
+
+	// deploy deployment
+	for nodeID, machines := range nodeMachineMap {
+		vms := []workloads.VM{}
+		QSFSs := []workloads.QSFS{}
+		disks := []workloads.Disk{}
+		for _, machine := range machines {
+			vm, disks, qsfss := generateVM(machine)
+			qsfs := generateQSFS(machine)
+			disks := generateDisk(machine)
+			vms = append(vms, vm)
+			//...
+		}
+		clientDeployment := workloads.NewDeployment(model.Name, nodeID, "", nil, model.Network.Name, disks, nil, vms, QSFSs)
+		err := client.DeploymentDeployer.Deploy(ctx, &clientDeployment)
+
+	}
+	model.Network.WireguardConfig = znet.AccessWGConfig
+	client.State.LoadVMFromGrid()
+	/*
+		- validate incoming deployment
+			- project name has to be unique
+			-
+		- construct network deployer
+			- get nodes from all machines
+			- build network deployer using these nodes
+		- deploy network
+		- construct deployment deployer
+			-
+		- deploy deployment
+		- construct machines model and return it
+	*/
+
+}
 
 // machines.deelte
 func MachinesDelete(ctx context.Context, name string) error
