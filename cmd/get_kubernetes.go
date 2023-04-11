4609130dbbcb4ecb0ff9edfa557841d65138c8fb
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-04-11 17:22:07 +0200

    Add get command for k8s and update get commands with grid3 client state

--- cmd/get_kubernetes.go
+++ cmd/get_kubernetes.go
@@ -13,10 +13,11 @@ import (
 	"github.com/threefoldtech/tf-grid-cli/internal/config"
 )
 
-// getGatewayNameCmd represents the get gateway name command
-var getGatewayNameCmd = &cobra.Command{
-	Use:   "name",
-	Short: "Get deployed gateway name",
+// getKubernetesCmd represents the get kubernetes command
+var getKubernetesCmd = &cobra.Command{
+	Use:   "kubernetes",
+	Short: "Get deployed kubernetes",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		cfg, err := config.GetUserConfig()
 		if err != nil {
@@ -30,42 +31,41 @@ var getGatewayNameCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
-		var nodeID uint32
-		var contractID uint64
+		var nodeIDs []uint32
 		for _, contract := range contracts.NodeContracts {
 			var deploymentData workloads.DeploymentData
 			err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
 			if err != nil {
 				log.Fatal().Err(err).Send()
 			}
-			if deploymentData.Type != "Gateway Name" || deploymentData.Name != args[0] {
+			if deploymentData.Type != "kubernetes" || deploymentData.Name != args[0] {
 				continue
 			}
-			nodeID = contract.NodeID
-			contractID, err = strconv.ParseUint(contract.ContractID, 0, 64)
+			nodeIDs = append(nodeIDs, contract.NodeID)
+			contractID, err := strconv.ParseUint(contract.ContractID, 0, 64)
 			if err != nil {
 				log.Fatal().Err(err).Send()
 			}
-
-			t.State.CurrentNodeDeployments[nodeID] = []uint64{contractID}
-			break
+			t.State.CurrentNodeDeployments[contract.NodeID] = append(t.State.CurrentNodeDeployments[contract.NodeID], contractID)
 		}
-		if nodeID == 0 {
-			log.Info().Msgf("no gateway name with name %s found", args[0])
+		if nodeIDs == nil {
+			log.Info().Msgf("no kubernetes cluster with name %s found", args[0])
 			os.Exit(0)
 		}
-		gateway, err := t.State.LoadGatewayNameFromGrid(nodeID, args[0], args[0])
+
+		vm, err := t.State.LoadK8sFromGrid(nodeIDs, args[0])
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
-		s, err := json.MarshalIndent(gateway, "", "\t")
+		s, err := json.MarshalIndent(vm, "", "\t")
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
-		log.Info().Msg("gateway name:\n" + string(s))
+		log.Info().Msg("k8s cluster:\n" + string(s))
+
 	},
 }
 
 func init() {
-	getGatewayCmd.AddCommand(getGatewayNameCmd)
+	getCmd.AddCommand(getKubernetesCmd)
 }
--- cmd/get_vm.go
+++ cmd/get_vm.go
@@ -3,10 +3,13 @@ package cmd
 
 import (
 	"encoding/json"
+	"os"
+	"strconv"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/threefoldtech/grid3-go/deployer"
+	"github.com/threefoldtech/grid3-go/workloads"
 	"github.com/threefoldtech/tf-grid-cli/internal/config"
 )
 
@@ -20,11 +23,39 @@ var getVMCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
-		t, err := deployer.NewTFPluginClient(cfg.Mnemonics, "sr25519", cfg.Network, "", "", "", true, false)
+		t, err := deployer.NewTFPluginClient(cfg.Mnemonics, "sr25519", cfg.Network, "", "", "", 100, true, false)
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
-		vm, err := t.GetVM(args[0])
+		contracts, err := t.ContractsGetter.ListContractsOfProjectName(args[0])
+		if err != nil {
+			log.Fatal().Err(err).Send()
+		}
+		var nodeID uint32
+		var contractID uint64
+		for _, contract := range contracts.NodeContracts {
+			var deploymentData workloads.DeploymentData
+			err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
+			if err != nil {
+				log.Fatal().Err(err).Send()
+			}
+			if deploymentData.Type != "vm" || deploymentData.ProjectName != args[0] {
+				continue
+			}
+			nodeID = contract.NodeID
+			contractID, err = strconv.ParseUint(contract.ContractID, 0, 64)
+			if err != nil {
+				log.Fatal().Err(err).Send()
+			}
+
+			t.State.CurrentNodeDeployments[nodeID] = []uint64{contractID}
+			break
+		}
+		if nodeID == 0 {
+			log.Info().Msgf("no vm with name %s found", args[0])
+			os.Exit(0)
+		}
+		vm, err := t.State.LoadDeploymentFromGrid(nodeID, args[0])
 		if err != nil {
 			log.Fatal().Err(err).Send()
 		}
